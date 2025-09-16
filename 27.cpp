6bf75470b10c38c522af870acf6e7de01bd85695
Joshua CHOI Kui Wang <94032950+joshuaSYSS@users.noreply.github.com>
2025-09-16 16:38:20 +0800

    add solutions

---
+++ 27.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long; using ld = long double;
+using ull = unsigned long long; using str = string;
+typedef vector<ll> vl;
+typedef vector<ld> vd;
+typedef set<ll> sl;
+typedef vector<vector<ll>> vl2;
+typedef vector<str> vs;
+typedef map<ll, ll> mll;
+typedef map<str, ll> msl;
+typedef queue<ll> ql;
+typedef priority_queue<ll> pqlg;
+typedef priority_queue<ll, vector<ll>, greater<ll>> pqls;
+typedef pair<ll, ll> pll;
+typedef vector<pair<ll, ll>> vpll;
+typedef queue<pair<ll, ll>> qpll;
+
+#define up(initial, n, step) for (ll i = (ll)(initial);i < (ll)(n);i += (ll)(step))
+#define up2(initial, n, step) for (ll j = (ll)(initial);j < (ll)(n);j += (ll)(step))
+#define up3(initial, n, step) for (ll k = (ll)(initial);k < (ll)(n);k += (ll)(step))
+#define down(initial, n, step) for (ll i = (ll)(initial) - 1;i >= (ll)(n);i -= (ll)(step))
+#define down2(initial, n, step) for (ll j = (ll)(initial) - 1;j >= (ll)(n);j -= (ll)(step))
+#define down3(initial, n, step) for (ll k = (ll)(initial) - 1;k >= (ll)(n);k -= (ll)(step))
+
+#define all(x) (x).begin(), (x).end()
+#define YES(a) ((a)?"YES":"NO")
+#define Yes(a) ((a)?"Yes":"No")
+#define yes(a) ((a)?"yes":"no")
+#define rev(s) reverse((s).begin(), (s).end());
+#define toStr(s) to_string((s))
+#define throwErr(s) throw invalid_argument(s)
+#define nextPerm next_permutation
+#define foreach(a, v) for(auto& a : v)
+#define tcin \
+  ll testcase; cin >> testcase;\
+  while (testcase--)
+
+#define LL_MAX 9223372036854775807
+#define LL_MIN (LL_MAX + 1)
+ll a, b, c, n;
+vl visited(n, 0);
+ll dfs(ll i) {
+    visited[i] = 1;
+    ll node = (a * i * i + b * i + c) % n;
+    if (visited[node]) {
+        return 1;
+    }
+    return dfs(node) + 1;
+}
+int main(void){
+std::ios::sync_with_stdio(EXIT_SUCCESS); std::cin.tie(EXIT_SUCCESS); std::cout.tie(EXIT_SUCCESS);
+tcin{
+    cin >> a >> b >> c >> n;
+ll res = 0;
+up(0, n, 1) {
+    visited = vl(n, 0);
+    res = max(res, dfs(i));
+}
+cout << res << '\n';
+}
+return 0;
+}
