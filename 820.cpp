a39d0480d40b14e51020ce9ed3eae7387fcd3c1c
Joshua CHOI Kui Wang <94032950+joshuaSYSS@users.noreply.github.com>
2025-09-22 12:21:19 +0800

    Implement DFS solution for counting valid sequences
    Added a depth-first search (dfs) function and supporting data structures to count the number of valid sequences based on 'cut' and 'nocut' sets. The main function now reads input, builds the tree structure, and outputs the count of valid sequences.

--- 820.cpp
+++ 820.cpp
@@ -38,9 +38,69 @@ typedef queue<pair<ll, ll>> qpll;
 
 #define LL_MAX 9223372036854775807
 #define LL_MIN (LL_MAX + 1)
-
+map<vl, vl> mp;
+set<vl> cut, nocut;
+pll dfs(vl i) { //{val, has nocut}
+    if (cut.count(i)) {
+        return { 1, 0 };
+    }
+    else if (nocut.count(i)) {
+        return { 0, 1 };
+    }
+    pll res = { 0, 0 };
+    foreach(x, mp[i]) {
+        vl a = i;
+        a.push_back(x);
+        pll b = dfs(a);
+        res.second |= b.second;
+        res.first += b.first;
+    }
+    if (!res.second) {
+        res.first = 1;
+    }
+    return res;
+}
 int main(void){
 std::ios::sync_with_stdio(EXIT_SUCCESS); std::cin.tie(EXIT_SUCCESS); std::cout.tie(EXIT_SUCCESS);
-
+tcin{
+    ll n, m; cin >> n >> m;
+mp.clear();
+cut.clear();
+nocut.clear();
+up(0, n, 1) {
+    vl v;
+    ll u;
+    while (cin >> u) {
+        if (u == -1) break;
+        ll has = 0;
+        foreach(x, mp[v]) {
+            if (x == u) has = 1;
+        }
+        if(!has) mp[v].push_back(u);
+        v.push_back(u);
+    }
+    cut.insert(v);
+}
+up(0, m, 1) {
+    vl v;
+    ll u;
+    while (cin >> u) {
+        if (u == -1) break;
+        ll has = 0;
+        foreach(x, mp[v]) {
+            if (x == u) has = 1;
+        }
+        if(!has) mp[v].push_back(u);
+        v.push_back(u);
+    }
+    nocut.insert(v);
+}
+ll cnt = 0;
+foreach(x, mp[{}]) {
+    pll u = dfs({ x });
+    cnt += u.first;
+}
+cout << cnt << '\n';
+}
 return 0;
 }
